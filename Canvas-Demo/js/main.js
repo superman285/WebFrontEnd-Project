130e426a1ed874079c5dd1afaae33761499d74a5
superman285 <superman285@126.com>
2018-10-16 12:52:07 +0800

    2018.10.16 Canvas-Demo 支持移动端版本ver2.0 commit

--- Canvas-Demo/js/main.js
+++ Canvas-Demo/js/main.js
@@ -4,7 +4,6 @@ var lineWidth = 2;
 
 //鼠标按下标记
 var mousedownOnOff = false;
-var eraserMousedown = false;
 
 //画笔开关，橡皮擦开关
 var brushSwitch = true;
@@ -18,43 +17,43 @@ var paintEraser = document.querySelector(".icon-eraser");
 var paintClear = document.querySelector(".icon-clear");
 var paintSave = document.querySelector(".icon-save");
 
-paintBrush.onclick = function (event) {
+paintBrush.onclick = function () {
     paintBrush.classList.add('active');
     paintEraser.classList.remove('active');
     brushSwitch = true;
     eraserSwitch = false;
-}
+};
 
-paintEraser.onclick = function (event) {
+paintEraser.onclick = function () {
     paintEraser.classList.add('active');
     paintBrush.classList.remove('active');
     brushSwitch = false;
     eraserSwitch = true;
-}
+};
 
-paintClear.onmousedown = function(event) {
+paintClear.onmousedown = function() {
     paintClear.classList.toggle('active2');
-}
-paintClear.onmouseup = function (event) {
+};
+paintClear.onmouseup = function () {
     paintClear.classList.toggle('active2');
-}
-paintClear.onclick = function (event) {
+};
+paintClear.onclick = function () {
     if(confirm("确认清空画布吗？")){
         clearCanvas(myCanvas);
     }else {
         //do nothing
     }
-}
+};
 
-paintSave.onmousedown = function(event) {
+paintSave.onmousedown = function() {
     paintSave.classList.toggle('active2');
-}
-paintSave.onmouseup = function (event) {
+};
+paintSave.onmouseup = function () {
     paintSave.classList.toggle('active2');
-}
-paintSave.onclick = function (event) {
+};
+paintSave.onclick = function () {
     saveCanvas(myCanvas);
-}
+};
 
 //画笔粗细
 var thinBrush = document.querySelector(".paintLineWidth li:first-child");
@@ -64,52 +63,56 @@ thinBrush.onclick = function(){
     lineWidth = 2;
     pThickIco.classList.remove('PTactive');
     pThinIco.classList.add('PTactive');
-}
+};
 
 thickBrush.onclick = function(){
     lineWidth = 5;
     pThinIco.classList.remove('PTactive');
     pThickIco.classList.add('PTactive');
-}
+};
 
 //画笔颜色
 var brushColor = 'black';
 
-paintBlack.onclick = function (event) {
+paintBlack.onclick = function () {
     paintBlack.classList.add('active');
     paintRed.classList.remove('active');
     paintGreen.classList.remove('active');
     paintBlue.classList.remove('active');
     brushColor = 'black';
-}
-paintRed.onclick = function (event) {
+};
+paintRed.onclick = function () {
     paintBlack.classList.remove('active');
     paintRed.classList.add('active');
     paintGreen.classList.remove('active');
     paintBlue.classList.remove('active');
     brushColor = 'red';
-}
-paintGreen.onclick = function (event) {
+};
+paintGreen.onclick = function () {
     paintBlack.classList.remove('active');
     paintRed.classList.remove('active');
     paintGreen.classList.add('active');
     paintBlue.classList.remove('active');
     brushColor = 'green';
-}
-paintBlue.onclick = function (event) {
+};
+paintBlue.onclick = function () {
     paintBlack.classList.remove('active');
     paintRed.classList.remove('active');
     paintGreen.classList.remove('active');
     paintBlue.classList.add('active');
     brushColor = 'blue';
-}
+};
+
+//设备 特性检测
+var isTouchMobile = 'ontouchstart' in window || navigator.userAgent.match(/(iPhone|iPod|Android|ios|Mobile)/i);
+
 
 //正文
 //设置画布大小
 setCanvasSize();
 window.onresize = function () {
     setCanvasSize();
-}
+};
 
 setCanvasbg(myCanvas);
 watchAction(myCanvas);
@@ -127,10 +130,10 @@ function setCanvasbg(canvas) {
 //设置画布大小函数
 function setCanvasSize() {
     var pageWidth = document.documentElement.clientWidth;
-    var pageHeight = document.documentElement.clientHeight
+    var pageHeight = document.documentElement.clientHeight;
 
-    myCanvas.width = pageWidth
-    myCanvas.height = pageHeight
+    myCanvas.width = pageWidth;
+    myCanvas.height = pageHeight;
 }
 
 //画圆
@@ -164,19 +167,33 @@ function clearCanvas(canvas) {
 //保存画布as图片到本地
 function saveCanvas(canvas) {
     var saveLink = document.createElement("a");
+    //未加入时也可生效
+    document.body.appendChild(saveLink);
     saveLink.href = canvas.toDataURL('image/png');
     saveLink.download = 'myBeautyCanvas';//必须设置download属性，a链接才有点击下载功效
+    saveLink.target = '_blank';
     saveLink.click();
 }
 
 //监听玩家操作
 function watchAction(canvas) {
+
+    var startEvent = isTouchMobile ? 'touchstart':'mousedown';
+    var moveEvent = isTouchMobile ? 'touchmove':'mousemove';
+    var endEvent = isTouchMobile ? 'touchend':'mouseup';
+
+    console.log(startEvent);
+    console.log(moveEvent);
+    console.log(endEvent);
+
     var lastX,lastY = undefined;
-    canvas.onmousedown = function (event) {
+
+    canvas.addEventListener(startEvent,(event)=>{
         console.log('点击画布事件触发');
         mousedownOnOff = true;
-        var x = event.clientX;
-        var y = event.clientY;
+        var x = event.clientX || event.touches[0].clientX;
+        var y = event.clientY || event.touches[0].clientY;
+        console.log('坐标'+x);
         if (brushSwitch) {
             drawCircle(x,y,lineWidth/2);
             lastX = x;
@@ -185,13 +202,13 @@ function watchAction(canvas) {
             // eraserPointer.style = `display:block;top:${y-10}px;left:${x-10}px`;
             eraseLine(x,y)
         }
-    }
-    canvas.onmousemove = function (event) {
+    });
+    canvas.addEventListener(moveEvent,function(event){
         var newX,newY;
         if (brushSwitch && mousedownOnOff) {
             console.log('点击鼠标后在画布上移动事件触发');
-            newX = event.clientX;
-            newY = event.clientY;
+            newX = event.clientX || event.touches[0].clientX;
+            newY = event.clientY || event.touches[0].clientY;
             console.log(newX);
             drawLine(lastX,lastY,newX,newY);
             drawCircle(lastX,lastY,lineWidth/2.5);
@@ -200,16 +217,23 @@ function watchAction(canvas) {
         }else if(eraserSwitch && mousedownOnOff) {
             //鼠标移动一次调用一次以下行为，所以一移动就擦
             console.log('橡皮擦，鼠标按下开动');
-            var erX = event.clientX;
-            var erY = event.clientY;
+            var erX = event.clientX || event.touches[0].clientX;
+            var erY = event.clientY || event.touches[0].clientY;
             // eraserPointer.style = `display:block;top:${erY-10}px;left:${erX-10}px`;
             eraseLine(erX,erY);
         }
-    }
-    canvas.onmouseup = function (event) {
+    });
+   /* canvas.onmousemove = function (event) {
+
+    }*/
+    canvas.addEventListener(endEvent,()=>{
         mousedownOnOff = false;
-        console.log('鼠标提起了');
-    }
+        if(isTouchMobile){
+            console.log('手指提起了');
+        }else {
+            console.log('鼠标提起了');
+        }
+    });
 
 }
 
